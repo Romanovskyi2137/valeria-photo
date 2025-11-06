ae40faca4b041cebb1e7b8b9436b25a8d46e8a86
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2025-11-06 13:18:54 +0200

    footer done, header fixed

--- template-parts/partials/partial-footer-main.php
+++ template-parts/partials/partial-footer-main.php
@@ -1,4 +1,11 @@
-
+<?php 
+    $ID = get_page_by_path('settings-page')->ID;
+    $location = get_field('location', $ID);
+    $email = get_field('email', $ID);
+    $instagram_link = get_field('instagram_link', $ID);
+    $raw_contact_number = get_field('contact_number', $ID);
+    $contact_number = preg_replace('/[^0-9+]/', '', $raw_contact_number);
+?>
 
 
 
@@ -7,8 +14,37 @@
         <div class="inquire_container">
             <a>inquire</a>
         </div>
-        <div class="contacts_container">contacts</div>
-        <div class="socials_container">instagram</div>
+        <div class="contacts_container">
+            <?php if ($location): ?>
+                <p><?php echo ($location); ?></p>
+            <?php endif; ?>
+            <?php if ($email): ?>
+                <a 
+                    rel="mailto"
+                    href="mailto:<?php echo ($email); ?>"
+                >
+                    <?php echo ($email); ?>
+                </a>
+            <?php endif; ?>
+            <?php if ($contact_number): ?>
+                <a 
+                    rel="tel"
+                    href="tel:<?php echo ($contact_number); ?>"
+                >
+                    <?php echo ($raw_contact_number); ?>
+                </a>
+            <?php endif; ?>    
+        </div>
+        <div class="socials_container">
+            <?php if ($instagram_link): ?>
+                <a
+                    href="<?php echo get_field('instagram_link', $ID); ?>"
+                    target="_blank"
+                >
+                    instagram
+                </a>
+            <?php endif; ?>
+        </div>
     </div>
     <p>
         © <?php echo date("Y"); ?> Poloz Valeriia Photography | All rights reserved.
