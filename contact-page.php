9a5cd820bc8db859491d796b3280533d34c74a5d
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2025-11-28 14:18:01 +0200

    final changes

--- contact-page.php
+++ contact-page.php
@@ -2,6 +2,14 @@
     // Template Name: Contact Page
 ?>
     <main>
+    <?php 
+        $photo = get_field('page_main_photo');
+        if ($photo) :
+    ?>
+        <div class="photo_container">
+        <img src="<?php echo $photo ?>"/>
+        </div> 
+    <?php endif ?>
         <?php echo do_shortcode( '[contact-form-7 id="3d8fe33" title="Inquire form"]' ); ?>
     </main>
 <?php get_footer(); ?>
